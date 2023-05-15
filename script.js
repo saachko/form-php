ee601d1c4c1444119580cce6b58e7ad08921434a
Anastasiya Sachko <95384801+saachko@users.noreply.github.com>
2023-05-15 03:06:17 +0300

    refactor the script

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
+$(document).ready(() => {
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+    const th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data: th.serialize(),
+      error: (jqXHR, exception) => {
+        alert('Error!');
+      },
+    }).done(function () {
+      alert('Thank you!');
+      setTimeout(function () {
+        th.trigger('reset');
+      }, 1000);
+    });
+    return false;
+  });
 });
